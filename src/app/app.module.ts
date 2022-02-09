1bff3daf6fee75cd5bca3a2dd11794bf618eda05
ActiveZ <activez.git@gmail.com>
2022-02-09 23:12:32 +0100

    accueil component + uppercase pseudo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { AvisComponent } from './components/avis/avis.component';
 import { CollegueComponent } from './components/collegue/collegue.component';
 import { ListeColleguesComponent } from './components/liste-collegues/liste-collegues.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { ScorePipe } from './pipes/score.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     AvisComponent,
     CollegueComponent,
-    ListeColleguesComponent
+    ListeColleguesComponent,
+    AccueilComponent,
+    ScorePipe
   ],
   imports: [
     BrowserModule
