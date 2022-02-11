956dfa06788801a3c05d85f4ef2d074f6abb9531
ActiveZ <activez.git@gmail.com>
2022-02-11 18:05:45 +0100

    validation synchrone ok

--- src/app/validators/nom-prenom-validator.directive.ts
+++ src/app/validators/nom-prenom-validator.directive.ts
@@ -1,10 +1,15 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors } from '@angular/forms';
 
 @Directive({
-  selector: '[appNomPrenomValidator]'
+  selector: '[appNomPrenomValidator]',
+  providers: [{provide: NG_VALIDATORS, useExisting: NomPrenomValidatorDirective,
+    multi: true}]
 })
 export class NomPrenomValidatorDirective {
+  constructor() {}
 
-  constructor() { }
-
+  validate(control: AbstractControl): ValidationErrors | null {
+    return control.value.nom != control.value.prenom ? null : { nomPrenomIdentique: true };
+  }
 }
